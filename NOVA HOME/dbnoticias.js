454d1178df60944e04ac60f2b6335bf5eec720a6
Gabriela Colem Castelo Borges <73849139+gabrielacolen@users.noreply.github.com>
2022-06-09 17:24:03 -0300

    Update dbnoticias.js

--- NOVA HOME/dbnoticias.js
+++ NOVA HOME/dbnoticias.js
@@ -3,20 +3,20 @@ var db = {
         {
             titulo: 'Samu orienta 115 moradores de morros em Santos sobre primeiros socorros',
             referencia: 'Prefeitura de Santos',
-            corpo: 'O intuito foi capacitar e aproximar os moradores, principalmente de áreas de difícil acesso para as ambulâncias. Eles foram orientados sobre como agir nos principais casos de emergência de saúde e receberam orientações a respeito do funcionamento dos serviços do Samu.'
-            //datas: '12/05/2022',
+            corpo: 'O intuito foi capacitar e aproximar os moradores, principalmente de áreas de difícil acesso para as ambulâncias. Eles foram orientados sobre como agir nos principais casos de emergência de saúde e receberam orientações a respeito do funcionamento dos serviços do Samu.',
+            datas: '12/05/2022',
             //url: 'https://www.santos.sp.gov.br/?q=noticia/samu-orienta-115-moradores-de-morros-em-santos-sobre-primeiros-socorros'
         }, {
             titulo: 'Confira 12 itens de primeiros socorros que você deve ter em casa',
             referencia: 'Jornal Metrópoles',
-            corpo: 'Alguns produtos não costumam estar nos kits que as pessoas têm em casa, mas valem o investimento. Um abaixador de língua, por exemplo, pode ajudar a ver algum objeto na garganta e até a imobilizar um dedo da mão. Outra boa aquisição é uma bolsa térmica, que pode ser usada tanto fria quanto quente e serve para cólicas ou mesmo hematomas ou inchaços por pancadas.'
-           // datas: '16/05/2022',
+            corpo: 'Alguns produtos não costumam estar nos kits que as pessoas têm em casa, mas valem o investimento. Um abaixador de língua, por exemplo, pode ajudar a ver algum objeto na garganta e até a imobilizar um dedo da mão. Outra boa aquisição é uma bolsa térmica, que pode ser usada tanto fria quanto quente e serve para cólicas ou mesmo hematomas ou inchaços por pancadas.',
+            datas: '16/05/2022',
            // url: 'https://www.metropoles.com/saude/confira-12-itens-de-primeiros-socorros-que-voce-deve-ter-em-casa'
     }, {
             titulo: 'iFood e Prefeitura realizam capacitação em primeiros socorros para entregadores de Salvador',
             referencia: 'Jornal Correio',
-            corpo: 'A capacitação ocorreu no Salvador Shopping, no Instituto JCPM de Compromisso Social (Espaço IJCPM), como parte das ações de conscientização que marcam o Maio Amarelo.'
-            //publicado: '27/05/2022',
+            corpo: 'A capacitação ocorreu no Salvador Shopping, no Instituto JCPM de Compromisso Social (Espaço IJCPM), como parte das ações de conscientização que marcam o Maio Amarelo.',
+            datas: '27/05/2022',
            //URL: 'https://www.correio24horas.com.br/noticia/nid/ifood-e-prefeitura-realizam-capacitacao-em-primeiros-socorros-para-entregadores-de-salvador/'
     },{
             titulo: 'Criança de um ano e meio se engasga com maçã e é salva por policial em Maracanaú',
